f7ea00d77687b1d26668c6fe094fa14e75f62954
MuraliKrishna <muralikrishna.theeda@gmail.com>
2025-03-18 16:02:44 +0530

    changed allowed hosts (http)

--- videoconferencing/videoconferencing/settings.py
+++ videoconferencing/videoconferencing/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-v!p5x(%jb4y%!ws52@h5%g$)io8r7sc39=k@b#d#zg#vf3l0^n
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '3.108.185.189',
+    '*',
 ]
+
 
 
 # Application definition
